d8f95b4bd4bdc7d95abf23f6abcdf5a0abbf1780
blinh513 <baolinh5132003@gmail.com>
2024-07-09 08:28:19 +0700

    add image to pistol prefhab

---
+++ Assets/Scripts/GunPoolScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Entity;
+
+public class GunPoolScript : MonoBehaviour
+{
+    public static GunPoolScript instance;
+    public List<GameObject> ImageGun;
+    public int Damage;
+    public float TimeReload;
+    public int SpeedBullet;
+    public int AmountBullet;
+    public AudioClip Reload;
+    public AudioClip Fire;
+    public string Type;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
